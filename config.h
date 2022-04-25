27f1c3a359ce67f9da5863b0f29b6544abf3129a
Šarūnas Nejus <snejus@protonmail.com>
2022-04-25 11:20:18 +0100

    Update font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka Fixed:size=10:aspect=1.04:antialias=true:autohint=true";
+static char *font = "Iosevka Term:size=10:aspect=1.04:antialias=true:autohint=true";
 static int borderpx = 10;
 
 /*
